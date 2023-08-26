409194018fd16f6377f3ffa31d273430079186a7
Nasim Rahaman <nasim.rahaman@tuebingen.mpg.de>
2023-08-26 13:58:56 +0200

    - unbork

--- llamapi/__init__.py
+++ llamapi/__init__.py
@@ -12,6 +12,16 @@ REDIS_BACKEND_URL = os.environ.get(
 API_HOST = os.environ.get("LLAMAPI_API_HOST", "0.0.0.0")
 API_PORT = int(os.environ.get("LLAMAPI_API_PORT", "8000"))
 
+# Worker side configurations
+LLAMAPI_GLOBAL_HF_CACHE_DIRECTORY = os.environ.get(
+    "LLAMAPI_GLOBAL_HF_CACHE_DIRECTORY", "/tmp/huggingface_cache"
+)
+LLAMAPI_LOCAL_HF_CACHE_DIRECTORY = os.environ.get(
+    "LLAMAPI_LOCAL_HF_CACHE_DIRECTORY", "/tmp/huggingface_cache"
+)
+
+
 # Client side configurations
 # This assumes that the client portforwarded the server port to localhost
 HOST_URL = os.environ.get("LLAMAPI_HOST_URL", "http://localhost:8000")
+POLL_INTERVAL = int(os.environ.get("LLAMAPI_POLL_INTERVAL", 2))
